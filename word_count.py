43ae5aa347a33113a0d8b94a35d13f1ec1cd1ad2
Joyal Thomas <joyal@dexlock.com>
2020-10-08 13:32:45 +0530

    final commit

--- word_count.py
+++ word_count.py
@@ -1,3 +1,5 @@
+temp_array=[]
+
 #Opening file and copying the contents to a string variable in lowercase letter 
 content = open('text.txt', 'r').read().lower()
 
@@ -5,7 +7,9 @@ content = open('text.txt', 'r').read().lower()
 content_array=[''.join(c for c in word if c.isalpha() or c=="'") for word in content.split()]
 
 #print only the words with count=1 and delete all its instances when a word is checked 
-print("Unique words in the file are: ")
+# print(content_array)
+print("Count of each words in the file are: ")
 for i in content_array:
-    if content_array.count(i)==1:
-        print (i) 
+    if i not in temp_array:
+        print (i,": ",str(content_array.count(i)))
+        temp_array.append(i)
